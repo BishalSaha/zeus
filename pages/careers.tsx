5607918dc14e1abc91c2ba47c5672581ffbf4d89
Bishal Saha <bishal.saha@gullak.money>
2022-04-27 21:44:53 +0530

    Initail Boilerplate

---
+++ pages/careers.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from "next";
+import styles from "../styles/About.module.css";
+import { motion } from "framer-motion";
+
+const Careers: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+        whileHover={{
+          scale: 1.2,
+          transition: {
+            duration: 0.1,
+          },
+        }}
+      >
+        <h1 className="title">Careers!</h1>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Careers;
